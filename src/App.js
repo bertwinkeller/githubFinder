d41ad77fa88b1ef162717a4a75f172df7500140f
Bert Keller <bertwinkeller@gmail.com>
2020-10-23 12:14:55 -0700

    not found component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar.js'
-import Users from './components/Users.js'
-import Search from './components/Search.js'
 import Alert from './components/Alert.js'
 import About from './components/pages/About.js'
 import User from './components/User.js'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
 import GithubState from './context/github/githubState'
 import AlertState from './context/alert/alertState'
 
@@ -25,15 +25,10 @@ const App = () => {
           <div className="container">
             <Alert  />
             <Switch>
-              <Route exact path="/" render={props => (
-                <Fragment>
-                  <Search  />
-                  <Users />
-                </Fragment>
-              )} />
+              <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
               <Route exact path="/user/:login" component={User} /> 
-               
+               <Route component={NotFound}/>
             </Switch>
 
           </div>
